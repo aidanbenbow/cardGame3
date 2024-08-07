f1b4ca62c4398e2b816d99e962e01ae103ad0408
Aidan Benbow <aidanbenbow@gmail.com>
2024-08-07 10:51:45 +0100

    3com

--- routes/games.js
+++ routes/games.js
@@ -4,13 +4,6 @@ const router = express.Router()
 import guessImageHiscore from'../models/HiScores.js'
 
 
-import { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3'
-
-
-import {s3} from '../config/s3.js'
-
-const bucketname = process.env.AWS_BUCKET_NAME
-
 router.get('/guessImage', async (req,res)=>{
     let hiscores = [{
         name: 'aidan',
@@ -20,15 +13,6 @@ router.get('/guessImage', async (req,res)=>{
         name: 'corm',
         hiscore: '5'
     }]
-    
-
-    const params = {
-        Bucket: bucketname,
-        Key: 'background.png',
-    } 
-    const command = new GetObjectCommand(params)
-    let item = await s3.send(command)
-    
     
 
     res.render('games/guessImage', {scores: hiscores})
